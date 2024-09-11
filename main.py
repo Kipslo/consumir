b159ef6c1f329383eddd142476d11afd2da95fef
Kipslo <gbrlrdrgsdslvl@gmail.com>
2024-09-11 16:31:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import tkinter as tk
 import socket
 import customtkinter as ctk
+import sqlite3 as sql
+
+#class funcs():
+
 
 
 class application():
-    def __main__(self):
-
-
-
-
-
+    def __init__(self):
         self.root = tk.Tk()
-        self.root.mainloop()
+        self.initwindow()
 
+
+        self.root.mainloop()
+    def initwindow(self):
+        self.root.attributes("-fullscreen", True)
+        self.root.configure(background="#1f1f1f")
+
+    def connectconts(self):
+        self.conts = sql.connect("sql.db")
+        self.contscursor = self.conts.cursor()
+
+    def desconnectconts(self):
+        self.conts.close()
+
+    def createtables(self):
+        self.contscursor.execute("""CREATE TABLE IF NOT EXISTS Conts(
+                                 name VARCHAR(30) NOT NULL,
+                                 password VARCHAR(30) NOT NULL,
+                                 
+                                 
+                                 
+                                 
+                                 )""")
+        
 application()
