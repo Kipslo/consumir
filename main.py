f4de282d4a70ef34340dea2a417033b114e32190
Kipslo <gbrlrdrgsdslvl@gmail.com>
2024-09-11 16:10:23 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import socket
+import customtkinter as ctk
+
+
+class application():
+    def __main__(self):
+
+
+
+
+
+        self.root = tk.Tk()
+        self.root.mainloop()
+
+application()
