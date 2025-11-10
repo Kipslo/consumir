95d8e779a98033e5d5b761beea979783db4e3e1a
Kipslo <gbrlrdrgsdslvl@gmail.com>
2025-11-10 00:07:16 -0300

    +

--- product.py
+++ product.py
@@ -1,5 +1,22 @@
-import connect
+import connect, table
 
-class product():
-    def __init__(self, ):
-        
+class product(connect):
+    def __init__(self, name:str = "", tipe:str = "", category:str = "", price:str = "", printer:str = "", stock:str = "0"):
+        self.name, self.tipe, self.category, self.price, self.printer, self.stock = name, tipe, category, price, printer, stock
+    def get(self):
+        product_table = table("produtos", "Products")
+        where = []
+        values = []
+        if self.name != "":
+            where.append("name"); values.append(self.name)
+        if self.tipe != "":
+            where.append("type"); values.append(self.tipe)
+        if self.category != "":
+            where.append("category"); values.append(self.category)
+        if self.price != "":
+            where.append("price"); values.append(self.price)
+        if self.stock != "":
+            where.append("stock"); values.append(self.stock)
+        data = product_table.data(where= tuple(where), value=tuple(values))
+    def create(self):
+        pass
--- table.py
+++ table.py
@@ -1,12 +1,15 @@
 import connect
 class table(connect):
-    def __init__(self, db : str, name : str, columns : tuple, values: tuple):
+    def __init__(self, db : str, name : str, columns : tuple = ("", ), values: tuple = ("", )):
         self.db = db
         self.name = name
         self.columns= columns
         self.values = values
+        self.createPermission = False
+        if columns[0] != "" and values[0] != "":
+            self.createPermission = True
     def create(self):
-        if len(self.columns) == len(self.values) and self.db != "" and self.name != "":
+        if len(self.columns) == len(self.values) and self.db != "" and self.name != "" and self.createPermission:
             rows = []
             for i, j in enumerate(self.columns):
                 rows.append(f"{self.columns[i]} {self.values[i]}")
@@ -16,10 +19,24 @@ class table(connect):
             self.desconnect(self.db)
     def listcolumns(self):
         return self.columns
-    def data(self):
-        self.connect()
-        temp = self.execute(self.db, F"SELECT * FROM {self.name}")
+    def data(self, column: tuple= ("*", ), where:tuple = (), value: tuple= ()):
+        self.connect(self.db)
+        cod = self.name
+        if len(where) > 0:
+            for i, j in enumerate(where):
+                if i == 0:
+                    cod =  cod + f" WHERE {where[i]} = {value[i]}"
+                else:
+                    cod = cod + f" AND {where[i]} = {value[i]}"
+        columns = ""
+        for i, j in enumerate(column):
+            if i == 0:
+                columns = j
+            else:
+                columns = columns + f", {j}"
+        temp = self.execute(self.db, F"SELECT {columns} FROM {cod}")
         listen = []
         for i in temp:
             listen.append()
+        self.desconnect(self.db)
         return listen
